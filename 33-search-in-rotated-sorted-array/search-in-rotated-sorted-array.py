a4f9c947b7fe3cb5e2d80721ba7057998971adb5
Ark Tandon <arktandoncs@gmail.com>
2026-07-31 02:48:13 +0530

    Time: 0 ms (100.00%) | Memory: 19.6 MB (10.47%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -6,12 +6,12 @@ class Solution:
             if nums[mid]==target:
                 return mid
             if nums[low]<=nums[mid]:
-                if nums[low]<=target<nums[mid]:
+                if nums[low]<=target<=nums[mid]:
                     high=mid-1
                 else:
                     low=mid+1
             else:
-                if nums[mid]<target<=nums[high]:
+                if nums[mid]<=target<=nums[high]:
                     low=mid+1
                 else:
                     high=mid-1

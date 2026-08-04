a08e283c6453f4df2cf56fd08f8779599370e463
Ark Tandon <arktandoncs@gmail.com>
2026-08-05 00:58:30 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (75.01%) - LeetSync

---
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        low,high=0,len(arr)-1
+        while low<=high:
+            mid = low+(high-low)//2
+            if (arr[mid]-(mid+1))<k:
+                low=mid+1
+            else:
+                high=mid-1
+        return k+1+high
